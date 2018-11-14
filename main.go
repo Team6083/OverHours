742031c01263305e09e969ecf74d4bce74a861f2
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-11-15 02:25:20 +0800

    add web pages

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 )
 
 const DBPATH = "./foo.db"
-const webPort = 80;
+const webPort = 80
 
 func main() {
-	log.Print("OverHours Starting at",time.Now())
+	log.Print("OverHours Starting at", time.Now())
 	web := web.NewWeb()
-	go web.ServeWebInterface(webPort)
+	web.ServeWebInterface(webPort)
 }
