6aba4e43268dc460d31634fcb263948e2fb4f301
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-30 22:38:24 +0800

    fix rename uuid

--- models/user.go
+++ models/user.go
@@ -63,7 +63,7 @@ func (database *Database) GetUserByUserName(name string) (*User, error) {
 
 func (database *Database) GetUserByUID(uid string) (*User, error) {
 	user := User{}
-	err := database.DB.C("users").Find(bson.M{"uid": uid}).One(&user)
+	err := database.DB.C("users").Find(bson.M{"uuid": uid}).One(&user)
 	if err != nil {
 		return nil, err
 	}
