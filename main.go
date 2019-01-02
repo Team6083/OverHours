123fcca516365dc2ab744fc8db72f14bfee5ae3c
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-01-02 20:15:01 +0800

    add setting

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/Team6083/OverHours/models"
 	"github.com/Team6083/OverHours/web"
 	_ "github.com/mattn/go-sqlite3"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"os"
 	"strconv"
@@ -32,6 +34,18 @@ func main() {
 	database, err := models.OpenDataBase(host, user, pass, dbName)
 	if err != nil {
 		panic(err)
+	}
+
+	_, err = database.GetSetting()
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			_, err = database.SaveSetting(&models.Setting{Id: bson.NewObjectId()})
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
 	}
 
 	webServer := web.NewWeb(database)
