f576d601b746030fe28f3ae59688973644260ea3
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2020-01-24 15:00:35 +0800

    [fix] Fix tests

--- models/meeting_test.go
+++ models/meeting_test.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/mgo.v2/bson"
 	"log"
@@ -16,7 +15,7 @@ func TestDatabase_GetMeetingsBySeason(t *testing.T) {
 	participant["test"] = ParticipantData{UserId: "test", Leave: false, IsAdmin: false}
 
 	for i := 0; i < 10; i++ {
-		meeting := Meeting{fmt.Sprintf("meet%d", i), time.Now().Unix(), "seasonTest", "title", "desc", 0, time.Now().Unix(), 0, participant, bson.NewObjectId()}
+		meeting := Meeting{time.Now().Unix(), "seasonTest", "title", "desc", 0, time.Now().Unix(), 0, participant, bson.NewObjectId()}
 		_, err := Database.SaveMeeting(&meeting)
 		if err != nil {
 			panic(err)
@@ -40,7 +39,7 @@ func TestDatabase_DeleteMeeting(t *testing.T) {
 	participant := make(map[string]ParticipantData)
 	participant["test"] = ParticipantData{UserId: "test", Leave: false, IsAdmin: false}
 
-	meeting := Meeting{"m1", time.Now().Unix(), "seasonTest", "title", "desc", 0, time.Now().Unix(), 0, participant, bson.NewObjectId()}
+	meeting := Meeting{time.Now().Unix(), "seasonTest", "title", "desc", 0, time.Now().Unix(), 0, participant, bson.NewObjectId()}
 
 	_, err := database.SaveMeeting(&meeting)
 	if err != nil {
@@ -48,14 +47,14 @@ func TestDatabase_DeleteMeeting(t *testing.T) {
 	}
 	assert.Nil(t, err)
 
-	meeting2, err := database.GetMeetingByMeetId(meeting.MeetId)
+	meeting2, err := database.GetMeetingById(meeting.Id.Hex())
 	if err != nil {
 		panic(err)
 	}
 	assert.Nil(t, err)
 
 	assert.NotNil(t, meeting2)
-	assert.Equal(t, meeting2.MeetId, meeting.MeetId)
+	assert.Equal(t, meeting2.Id.Hex(), meeting.Id.Hex())
 
 	log.Print(meeting2.StartTime, meeting.StartTime)
 
