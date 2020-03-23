6b9170f07573946482f5f90ad1b50a0a49af34c8
Leafoliage <leafoliages@gmail.com>
2020-03-23 22:32:29 +0800

    [add] ifttt post request

--- web/meetings.go
+++ web/meetings.go
@@ -7,6 +7,7 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
+	"time"
 )
 
 func (web *Web) HandleMeetingRoutes(router *gin.Engine) {
@@ -20,6 +21,9 @@ func (web *Web) HandleMeetingRoutes(router *gin.Engine) {
 	meetingGroup := router.Group("/meeting")
 	meetingGroup.GET("/logs/:meetingId", web.APIGetMeetingLogs)
 	meetingGroup.PUT("/participants/:meetingId/:userId", web.APIPutMeetingParticipants)
+
+	iftttMeetingGroup := router.Group("/iftttMeetings")
+	iftttMeetingGroup.POST("/", web.IFTTTPostMeetings)
 }
 
 //APIHandler
@@ -45,6 +49,48 @@ func (web *Web) APIPostMeetings(ctx *gin.Context) {
 
 	if err := ctx.ShouldBind(&meeting); err != nil {
 		handleBadRequest(ctx, err)
+	}
+
+	change, err := web.database.SaveMeeting(&meeting)
+	if err != nil {
+		handleWebErr(ctx, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, change)
+}
+
+// POST /iftttMeetings
+func (web *Web) IFTTTPostMeetings(ctx *gin.Context) {
+	const dateLayout = "January 2, 2006 at 03:04PM"
+
+	type IFTTTInput struct {
+		Title          string `json:"title"`
+		Description    string `json:"description"`
+		EventStartTime string `json:"event_start_time"`
+		EventEndTime   string `json:"event_end_time"`
+		CreateTime     string `json:"create_time"`
+	}
+
+	var newIFTTT IFTTTInput
+	if err := ctx.ShouldBind(&newIFTTT); err != nil {
+		handleBadRequest(ctx, err)
+	}
+
+	startTime, _ := time.Parse(dateLayout, newIFTTT.CreateTime)
+	startCheckTime, _ := time.Parse(dateLayout, newIFTTT.EventStartTime)
+	finishTime, _ := time.Parse(dateLayout, newIFTTT.EventEndTime)
+
+	meeting := models.Meeting{
+		Id:               bson.NewObjectId(),
+		StartTime:        startTime.Unix(),
+		SeasonId:         web.settings.SeasonId,
+		Title:            newIFTTT.Title,
+		Description:      newIFTTT.Description,
+		CheckinLevel:     models.PermissionMember,
+		StartCheckinTime: startCheckTime.Add(-30 * time.Minute).Unix(),
+		FinishTime:       finishTime.Unix(),
+		//Participants: ,
 	}
 
 	change, err := web.database.SaveMeeting(&meeting)
