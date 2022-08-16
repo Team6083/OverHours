0d7d5b87407d7ac56d9b01d69e94b2e99a12a018
Leafoliage <leafoliages@gmail.com>
2022-08-16 21:05:39 +0800

    [add] swag for stat

--- web/stat.go
+++ web/stat.go
@@ -1,15 +1,34 @@
 package web
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type UserInfoPayload struct {
+	UserId    string  `json:"userId"`
+	TotalTime float64 `json:"totalTime"`
+}
+
+type StatUsersPayload struct {
+	Users    []UserInfoPayload `json:"users"`
+	SeasonId string            `json:"seasonId"`
+}
 
 func (web *Web) HandleStatRoutes(router *gin.Engine) {
 	statGroup := router.Group("/stat")
 	statGroup.GET("/users", web.APIGetStatUsers)
 }
 
+// APIGetStatUsers
+// @Router /stat/users [get]
+// @Summary Get stat users
+// @Tags stat
+// @Accept json
+// @Param seasonId path string true "seasonId"
+// @Produce json
+// @Success 200 {object} StatUsersPayload
 func (web *Web) APIGetStatUsers(ctx *gin.Context) {
 	seasonId := ctx.Query("seasonId")
 	if seasonId == "" {
@@ -22,20 +41,10 @@ func (web *Web) APIGetStatUsers(ctx *gin.Context) {
 		return
 	}
 
-	type UserInfoPayload struct {
-		UserId    string  `json:"userId"`
-		TotalTime float64 `json:"totalTime"`
-	}
-
 	var userInfos []UserInfoPayload
 
 	for _, v := range ranking {
 		userInfos = append(userInfos, UserInfoPayload{UserId: v.UserID, TotalTime: v.TotalTime.Seconds()})
-	}
-
-	type StatUsersPayload struct {
-		Users    []UserInfoPayload `json:"users"`
-		SeasonId string            `json:"seasonId"`
 	}
 
 	ctx.JSON(http.StatusOK, StatUsersPayload{SeasonId: seasonId, Users: userInfos})
